2bf5cbee500128a19a8d323051865e1d576efa54
Pax Klieber <pax.klieber87@gmail.com>
2024-01-21 03:49:52 +0000

    app header and configuration tweaks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import fs from 'fs'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+  server:{
+	  host: "172.232.173.160",
+//	  https: {
+//		  key: fs.readFileSync('/home/pax/example_com.key'),
+//		  cert: fs.readFileSync('/home/pax/rentsoundly_net.crt'),
+//	  }
+}
+}
+)
